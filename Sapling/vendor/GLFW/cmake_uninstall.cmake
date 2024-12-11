bf24a0bd7b3747f8c5b179a499eef75e1af8bb8f
filipclavin <filipclavin99@gmail.com>
2024-12-11 13:20:30 -0800

    Remove ignored files

--- Sapling/vendor/GLFW/cmake_uninstall.cmake
+++ Sapling/vendor/GLFW/cmake_uninstall.cmake
@@ -1,9 +1,9 @@
 
-if (NOT EXISTS "C:/src/Copy_Sapling/Sapling/vendor/GLFW/install_manifest.txt")
-    message(FATAL_ERROR "Cannot find install manifest: \"C:/src/Copy_Sapling/Sapling/vendor/GLFW/install_manifest.txt\"")
+if (NOT EXISTS "C:/src/Sapling/Sapling/vendor/GLFW/install_manifest.txt")
+    message(FATAL_ERROR "Cannot find install manifest: \"C:/src/Sapling/Sapling/vendor/GLFW/install_manifest.txt\"")
 endif()
 
-file(READ "C:/src/Copy_Sapling/Sapling/vendor/GLFW/install_manifest.txt" files)
+file(READ "C:/src/Sapling/Sapling/vendor/GLFW/install_manifest.txt" files)
 string(REGEX REPLACE "\n" ";" files "${files}")
 
 foreach (file ${files})
